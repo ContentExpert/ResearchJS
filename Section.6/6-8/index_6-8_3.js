a22d69f45b32eb77efe090753690865392c919b2
Seoyeol.IN <seoyeol.in@emcast.com>
2024-08-02 16:16:39 +0900

    240802 Udemy JS 6-8 코드 수정

--- Section.6/6-8/index_6-8_3.js
+++ Section.6/6-8/index_6-8_3.js
@@ -3,7 +3,7 @@ const second = [4, 5, 6];
 
 const combined = first.concat(second);
 
-const slice = combined.slice();
+const slice = combined.slice(2);
 
 console.log(combined); // [ 1, 2, 3, 4, 5, 6 ]
-console.log(slice); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(slice); // [ 3, 4, 5, 6 ]
--- Section.6/6-8/index_6-8_4.js
+++ Section.6/6-8/index_6-8_4.js
@@ -1,10 +1,9 @@
-const first = [{id: 1}];
+const first = [1, 2, 3];
 const second = [4, 5, 6];
 
 const combined = first.concat(second);
-first[0].id = 10;
 
 const slice = combined.slice();
 
-console.log(combined); // [ { id: 10 }, 4, 5, 6 ]
-console.log(slice); // [ { id: 10 }, 4, 5, 6 ]
+console.log(combined); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(slice); // [ 1, 2, 3, 4, 5, 6 ]
